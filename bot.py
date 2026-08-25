658867ec3ce3d797fec4a35f4275e64a2c1ab4bd
meddo707-bit <meddo707@hotmail.com>
2026-08-25 22:05:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,13 +3,11 @@ import requests
 import os
 BOT=os.getenv("BOT_TOKEN")
 CHAT=os.getenv("CHAT_ID")
-
 def rsi(d):
  a=d['Close'].diff()
  g=a.where(a>0,0).rolling(14).mean()
  l=-a.where(a<0,0).rolling(14).mean()
  return 100-(100/(1+g/l))
-
 def info(s):
  try:
   df=yf.download(s,period="1mo")
@@ -25,27 +23,8 @@ def info(s):
   return c,ch,r,lo,tg,st
  except:
   return None
-
-syms=[
-"2222.SR",
-"2010.SR",
-"META",
-"NVDA",
-"BTC-USD",
-"ETH-USD",
-"SOL-USD"
-]
-
-nms=[
-"2222",
-"2010",
-"META",
-"NVDA",
-"BTC",
-"ETH",
-"SOL"
-]
-
+syms=["2222.SR","2010.SR","META","NVDA","BTC-USD","ETH-USD","SOL-USD"]
+nms=["2222","2010","META","NVDA","BTC","ETH","SOL"]
 m="Bot\n\n"
 for i in range(7):
  d=info(syms[i])
@@ -56,7 +35,6 @@ for i in range(7):
  m+=f"{c:.2f} {ch:.2f}%\n"
  m+=f"RSI {r:.1f}\n"
  m+=f"T {tg:.2f} S {st:.2f}\n\n"
-
 m+="Live"
 url=f"https://api.telegram.org/bot{BOT}/sendMessage"
 requests.post(url,data={"chat_id":CHAT,"text":m})
